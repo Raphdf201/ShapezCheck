a3c2351c89d109121a4af5be0ab1c5b1dc9e34ff
Raphael Deschenes <deschenesr545@gmail.com>
2024-10-29 15:39:30 -0400

    Removed search and lookup functions

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,3 @@ fn main() {
     println!("Welcome to the shape checker by Raphdf201");
     let args: Vec<String> = env::args().collect();
 }
-
-fn lookup(s: char[]) {
-
-}
-
-fn search() -> char[] {
-    
-}
